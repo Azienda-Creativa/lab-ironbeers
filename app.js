143a5e7ee469cbba6cdeeb77a5dffc6101ccf1ba
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-02-04 18:09:11 +0100

    lab  is  working ( with Gonzalo )

--- app.js
+++ app.js
@@ -14,12 +14,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+hbs.registerPartials(__dirname + '/views');
+
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('pageBeer', { beersFromApi });
+    })
+
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log('Beers from the database: ', randomBeer);
+      res.render('randomPageBeer', { randomBeer });
+    })
+
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
